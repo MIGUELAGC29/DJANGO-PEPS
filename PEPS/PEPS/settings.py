8f545133a33c3e3fb686f2b03ef19dc477d669fb
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-03-17 21:05:09 -0600

    se agrega el crud ya realizado

--- PEPS/PEPS/settings.py
+++ PEPS/PEPS/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'PEPS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PEPS',
+        'USER': 'root',
+        'PASSWORD': 'PEDROagc29',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- PEPS/PEPS/urls.py
+++ PEPS/PEPS/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('is/', include('Login.urls')),
+    path('crud/', include('Login.urls')),
 ]
