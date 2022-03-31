306423ca6d4a5e5b0458041102028f7dcef3c2ab
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-03-31 00:39:04 -0600

    Se arregla el listado de productos de cada almacen con todas sus caracteristicas

--- PEPS/App/views.py
+++ PEPS/App/views.py
@@ -9,50 +9,36 @@ def Home(request):
     #cuantos almacenes tiene
     #obtener los productos de cada almacen
     if request.method == "POST" or request.method == "GET":
-        print('Holi soy muy feliz')
         username = request.user.username
-        print(username)
         ide = request.user.id
-        print('ide: ' + str(ide))
         almacenes = Almacen.objects.filter(usuario_id = ide)
         num_almacen = len(almacenes)
-        print(num_almacen)
-        print('--------------------')
-        nombre_productos = {}
+        d = {}
+        precio = {}
+        costo = {}
+        
         
         for almacen in almacenes:
-            lista_nombre_productos = []
-            lista_precio_productos = []
-            lista_costo_productos = []
-            lista_unidades_productos = []
-            lista_descripcion_productos = []
-            lista_proveedor_productos = []
-            #print(almacen.id)
-            producto_almacen = Producto.objects.filter(Almacen_id = almacen.id)
-            for producto in producto_almacen:
-                lista_nombre_productos.append(producto.nombre)
-                lista_precio_productos.append(producto.precio)
-                lista_costo_productos.append(producto.costo)
-                lista_unidades_productos.append(producto.unidades)
-                lista_descripcion_productos.append(producto.descripcion)
-                
-                
-            nombre_productos[almacen.id] = (lista_nombre_productos)
-        print(nombre_productos.values())
-            
-
-    
+            productos = Producto.objects.filter(Almacen_id = almacen.id)
+            d[almacen] = productos
+            lista_precio = []
+            lista_costo = []
+            resultado_precio = 0
+            resultado_costo = 0
+            for producto in productos:
+                lista_precio.append(producto.precio)
+                lista_costo.append(producto.costo)
+            for pre in lista_precio:
+                resultado_precio += int(pre)
+            for prec in lista_costo:
+                resultado_costo += int(prec)
+            precio[almacen] = resultado_precio
+            costo[almacen] = resultado_costo
+            lista_precio.clear()
+            lista_costo.clear()
         return render(request, 'home1.html', {'username':username,
                                              'almacenes':almacenes,
-                                             'nombre_productos':nombre_productos})
-           
+                                             'd':d,
+                                             'precio':precio,
+                                             'costo':costo})
         
-    else:
-        print(('Soy muy trsite'))
-    
-    """def listar_todo(request)
-		articulos = articles.objects.all()
-		return render(request, 'articulos.html', {'articulos':articulos})"""
-    
-    #return render(request, 'home1.html', {'almacenes':almacenes})
-    #return render(request, 'home1.html')
